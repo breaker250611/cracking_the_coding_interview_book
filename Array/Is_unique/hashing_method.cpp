27c5ce6568ee08985167a3d93c22acf3a341ec49
breaker250611 <dhruvtomer65@gmail.com>
2022-04-24 20:10:25 +0530

    testing

---
+++ Array/Is_unique/hashing_method.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+void yes(){
+    cout<<"YES"<<"\n";
+}
+void no(){
+    cout<<"NO"<<"\n";
+}
+
+void solve() {
+   
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int tc = 1;
+    // cin >> tc;
+    for (int t = 1; t <= tc; t++) {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
